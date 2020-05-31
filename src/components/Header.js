f105667cc0fae21a20f647dd04a30b467c25e11c
85153 <bolin1@students.cs.ubc.ca>
2020-05-31 13:35:49 -0700

    final -v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,50 @@
 import React, {Component} from 'react';
 import "../index.css"
 
-
 class Header extends Component {
+    // constructor() {
+    //     super();
+    //     // this.handleScroll = this.handleScroll.bind(this);
+    // }
+    //
+    // componentDidMount = () => {
+    //     window.addEventListener('scroll', this.handleScroll);
+    // }
+    //
+    // componentWillUnmount = () => {
+    //     window.removeEventListener('scroll', this.handleScroll);
+    // }
+    //
+    // handleScroll=(event)=> {
+    //     event.preventDefault()
+    //     const tar = document.querySelectorAll('.scroll');
+    //
+    //     let len = tar.length;
+    //     for (let i = 0; i < len; i++) {
+    //         let pos = window.pageYOffset * tar[i].dataset.rate;
+    //         if (tar[i].dataset.direction === 'vertical') {
+    //             this.setState({
+    //                 transform: "translate3d(0px," + pos + "px, 0px)"
+    //             })
+    //             //tar[i].style.transform = "translate3d(0px," + pos + "px, 0px)";
+    //         }
+    //         else{
+    //             //
+    //             this.setState({
+    //                 transform: "translate3d(0px," + pos + "px, 0px)"
+    //             })
+    //         }
+    //     }
+    //
+    // }
 
     render() {
         return(
-            <div id={"container-div"}>
-                <ul className={"title-ul"}>
-                    <li className={"scroll text"} data-rate={"-1.1"} data-direction={"vertical"}>SAY</li>
-                    <li className={"scroll text"} data-rate={"-1.5"} data-direction={"vertical"}>HI</li>
-                </ul>
+            <div id={"container-div"} >
+
             </div>
         )
     }
-
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from "./store";
 
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+            <App />
     </Provider>,
     document.getElementById('root'));
 
