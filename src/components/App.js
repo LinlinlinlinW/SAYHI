4603100efcfaa07da432edf571e5e736f088bdc1
85153 <bolin1@students.cs.ubc.ca>
2020-06-05 23:14:29 -0700

    try to add router

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,29 @@ import MessageInputCard from "./MessageInputCard";
 import MessageDisplayCard from "./MessageDisplayCard";
 import Navbar from "./Navbar";
 import Header from "./Header";
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App(){
     return (
-        <div className="App">
-            <Navbar />
-            <Header />
-            <MessageInputCard />
-            <MessageDisplayCard />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Header />
+                <Switch>
+                    <Route path={"../public/index"} exact component={Home} />
+                    <Route path={"../public/aboutme"} exact component={Header} />
+                </Switch>
+            </div>
+        </Router>
+
     );
 }
+
+const Home = () => (
+    <div>
+        <MessageInputCard />
+        <MessageDisplayCard />
+    </div>
+)
 
 export default App;
