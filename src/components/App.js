e6fcebb0f9e4d050b7b31cf7c60a31f735b73787
85153 <bolin1@students.cs.ubc.ca>
2020-06-07 13:15:35 -0700

    try to add db

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import "../index.css"
+import React, { Component } from "react";
+import "../index.css";
 import MessageInputCard from "./Home/MessageInputCard";
 import MessageDisplayCard from "./Home/MessageDisplayCard";
 import Navbar from "./Navbar";
 import Header from "./Header";
+import connection from "../connection/connection";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
-function App(){
-    return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Switch>
-                    <Route path={"/"} exact component={Home}/>
-                    <Route path={"/about"} exact component={About}/>
-                </Switch>
-            </div>
-        </Router>
-    );
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path={"/"} exact component={Home} />
+          <Route path={"/about"} exact component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 const Home = () => (
-    <div>
-        <Header />
-        <MessageInputCard />
-        <MessageDisplayCard />
-    </div>
-)
+  <div>
+    <Header />
+    <MessageInputCard />
+    <MessageDisplayCard />
+  </div>
+);
 
 const About = () => (
-    <div>
-        <h1>ABOUT ME</h1>
-    </div>
-)
+  <div>
+    <h1>ABOUT ME</h1>
+  </div>
+);
 
 export default App;
---
+++ src/connection/connection.js
@@ -0,0 +1,12 @@
+// const mongoose = require("mongoose");
+
+// let mongoDB =
+//   "mongodb+srv://m001-student:m001-mongodb-basics@sandbox-qheyf.mongodb.net/video?retryWrites=true&w=majority";
+
+// mongoose
+//   .connect(mongoDB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("connected to database"))
+//   .catch((err) => console.log("refuse to connect ", err));
