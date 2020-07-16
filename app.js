24ee15fb01316beff1b67e01751459d94a43cb80
85153 <bolin1@students.cs.ubc.ca>
2020-07-15 20:42:08 -0700

    change port to env port

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(80);
+app.listen(process.env.PORT || 80);
 // comments
